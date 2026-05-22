a3341cc2c94f59b3daad071d6fb3b38fe48d6672
balecore <balecored@gmail.com>
2026-05-22 14:44:25 +0500

    активити меин + Шаповалов

---
+++ app/src/main/java/com/example/pr29/presentations/BindingAdapters.java
@@ -0,0 +1,25 @@
+package com.example.pr29.presentations;
+
+import androidx.databinding.BindingAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pr29.domains.models.Day;
+
+import java.util.List;
+
+public class BindingAdapters {
+
+    @BindingAdapter("app:days")
+
+    public static void setDays(RecyclerView recyclerView, List<Day> days) {
+
+        if (recyclerView.getAdapter() instanceof DayAdapter) {
+
+            DayAdapter adapter = (DayAdapter) recyclerView.getAdapter();
+            adapter.setDays(days);
+
+        }
+
+    }
+
+}
